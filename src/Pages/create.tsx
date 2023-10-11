2ecdde892d8290b2f232a69779b23f6e5c0730ed
ngud-0119 <finaldev.dream119@gmail.com>
2023-10-11 05:11:27 -0400

    feat: validation about first name

--- src/Pages/create.tsx
+++ src/Pages/create.tsx
@@ -5,76 +5,196 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
+import { useForm } from "@refinedev/react-hook-form";
+import { HttpError } from "@refinedev/core";
+import { Controller } from "react-hook-form";
+
+import * as Yup from "yup";
+
+interface IFormValue {
+  firstname: string;
+  lastname: string;
+  address: string;
+  number: number;
+  work: string;
+  company: string;
+  role: string;
+}
+
+const schema = Yup.object().shape({
+  firstname: Yup.string().label("First Name").trim().required().min(3).max(64),
+  lastname: Yup.string().label("Last Name").trim().required().min(3).max(64),
+  address: Yup.string().label("Address").trim().required().min(3),
+  number: Yup.number().label("Number").required(),
+  work: Yup.string().label("Work").oneOf(["unemployed", "employed"]),
+  company: Yup.string().when("work", ([work], schema) => {
+    if (work === "employed") {
+      return schema.required().min(3).max(64);
+    }
+    return schema.notRequired();
+  }),
+  role: Yup.string().when("work", ([work], schema) => {
+    if (work === "employed") {
+      return schema.required().min(3).max(64);
+    }
+    return schema.notRequired();
+  }),
+});
+
+const defaultValues = {
+  firstname: "",
+  lastname: "",
+  address: "",
+  number: 0,
+  work: "unemployed",
+  company: "",
+  role: "",
+};
 
 const Create: React.FC = () => {
-    return (
-        <form style={{ display: "flex", flexDirection: "column" }}>
-            <TextField
-                fullWidth
-                sx={{ maxWidth: 600 }}
-                label="First Name"
-                margin="dense"
-            />
-            <TextField
-                fullWidth
-                sx={{ maxWidth: 600 }}
-                label="Last Name"
-                margin="dense"
-            />
-            <TextField
-                fullWidth
-                sx={{ maxWidth: 600 }}
-                label="Address"
-                margin="dense"
-            />
-            <TextField
-                fullWidth
-                sx={{ maxWidth: 600 }}
-                label="Number"
-                margin="dense"
-                type="number"
-            />
-            <FormControl sx={{ marginTop: 1, marginBottom: 0.7 }}>
-                <InputLabel id="type-label">Work</InputLabel>
-                <Select
-                    sx={{ maxWidth: 600 }}
-                    margin="dense"
-                    type="select"
-                    labelId="type-label"
-                    label="Work"
-                >
-                    <MenuItem value="employed">Employed</MenuItem>
-                    <MenuItem value="unemployed">Unemployed</MenuItem>
-                </Select>
-            </FormControl>
-            <TextField
-                fullWidth
-                sx={{ maxWidth: 600 }}
-                label="Company"
-                margin="dense"
-            />
-            <TextField
-                fullWidth
-                sx={{ maxWidth: 600 }}
-                label="Role"
-                margin="dense"
-            />
-            <Button
-                type="submit"
-                variant="contained"
-                fullWidth
-                sx={{
-                    maxWidth: "600px",
-                    padding: "10px",
-                    backgroundColor: "#67BE23",
-                    color: "white",
-                    marginTop: "5px",
-                }}
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    defaultValues,
+  });
+
+  const handleSubmission = (data: IFormValue) => console.log(data);
+
+  return (
+    <form
+      style={{ display: "flex", flexDirection: "column" }}
+      onSubmit={handleSubmit(handleSubmission)}
+    >
+      <Controller
+        control={control}
+        name="firstname"
+        rules={{ required: true, minLength: 5 }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            fullWidth
+            sx={{ maxWidth: 600 }}
+            label="First Name"
+            margin="dense"
+            error={!!errors.firstname}
+            helperText={errors.firstname && `${errors.firstname.message}`}
+          />
+        )}
+      />
+      {/* This is Validation code */}
+      {/* {errors.firstname && (
+        <span style={{ color: "red" }}>
+          You need to enter at least 5 characters!
+        </span>
+      )} */}
+      <Controller
+        control={control}
+        name="lastname"
+        render={({ field }) => (
+          <TextField
+            {...field}
+            fullWidth
+            sx={{ maxWidth: 600 }}
+            label="Last Name"
+            margin="dense"
+            error={!!errors.lastname}
+            helperText={errors.lastname && `${errors.lastname.message}`}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="address"
+        render={({ field }) => (
+          <TextField
+            {...field}
+            fullWidth
+            sx={{ maxWidth: 600 }}
+            label="Address"
+            margin="dense"
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="number"
+        render={({ field }) => (
+          <TextField
+            {...field}
+            fullWidth
+            sx={{ maxWidth: 600 }}
+            label="Number"
+            margin="dense"
+            type="number"
+          />
+        )}
+      />
+      <FormControl sx={{ marginTop: 1, marginBottom: 0.7 }}>
+        <InputLabel id="type-label">Work</InputLabel>
+        <Controller
+          control={control}
+          name="work"
+          render={({ field }) => (
+            <Select
+              sx={{ maxWidth: 600 }}
+              margin="dense"
+              {...field}
+              type="select"
+              labelId="type-label"
+              label="Work"
             >
-                Submit
-            </Button>
-        </form>
-    );
+              <MenuItem value="employed">Employed</MenuItem>
+              <MenuItem value="unemployed">Unemployed</MenuItem>
+            </Select>
+          )}
+        />
+      </FormControl>
+      <Controller
+        control={control}
+        name="company"
+        render={({ field }) => (
+          <TextField
+            {...field}
+            fullWidth
+            sx={{ maxWidth: 600 }}
+            label="Company"
+            margin="dense"
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="role"
+        render={({ field }) => (
+          <TextField
+            {...field}
+            fullWidth
+            sx={{ maxWidth: 600 }}
+            label="Role"
+            margin="dense"
+          />
+        )}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        fullWidth
+        sx={{
+          maxWidth: "600px",
+          padding: "10px",
+          backgroundColor: "#67BE23",
+          color: "white",
+          marginTop: "5px",
+        }}
+      >
+        Submit
+      </Button>
+    </form>
+  );
 };
 
 export default Create;
